09e1cd4d4d5949861508793af924de134ceac1c6
Jan Baer <janthebaer@gmail.com>
2023-12-07 10:24:59 +0100

    day3 finished

--- 03_gear_ratios/gear_ratios.go
+++ 03_gear_ratios/gear_ratios.go
@@ -27,7 +27,72 @@ func GearRatios() (int, int) {
 }
 
 func round2(file *os.File) int {
-	return 0
+	sum := 0
+	for lineNumber, line := range engineSchematic {
+		for characterIndex := 0; characterIndex < len(line); characterIndex++ {
+			char := line[characterIndex]
+			if string(char) == "*" {
+				adjacentNumbers := findAdjacentNumbers(lineNumber, characterIndex)
+				length := len(adjacentNumbers)
+				if length == 0 || length == 1 {
+					continue
+				} else {
+					gearRatio := 1
+					for _, value := range adjacentNumbers {
+						gearRatio *= value
+					}
+					sum += gearRatio
+				}
+			}
+
+		}
+	}
+
+	return sum
+}
+
+func findAdjacentNumbers(lineNumber, characterIndex int) []int {
+	var adjacentNumbers = make([]int, 0)
+	firstRow, lastRow, firstColumn, lastColumn := getCappedDimensions(lineNumber, characterIndex, 1)
+	for rowIndex := firstRow; rowIndex <= lastRow; rowIndex++ {
+		for columnIndex := firstColumn; columnIndex <= lastColumn; columnIndex++ {
+			char := engineSchematic[rowIndex][columnIndex]
+			if unicode.IsDigit(rune(char)) {
+				number, skipLength := getAdjacentNumber(rowIndex, columnIndex)
+				adjacentNumbers = append(adjacentNumbers, number)
+				columnIndex += skipLength
+			}
+		}
+	}
+	return adjacentNumbers
+}
+
+func getAdjacentNumber(rowIndex, columnIndex int) (int, int) {
+	numberString := ""
+	skipLength := 0
+	startIndex := columnIndex
+	// Find start of number
+	for i := columnIndex; i >= 0; i-- {
+		if unicode.IsNumber(rune(engineSchematic[rowIndex][i])) {
+			startIndex = i
+		} else {
+			break
+		}
+	}
+	for i := startIndex; i < len(engineSchematic[rowIndex]); i++ {
+		if !unicode.IsNumber(rune(engineSchematic[rowIndex][i])) {
+			break
+		}
+		numberString += string(engineSchematic[rowIndex][i])
+		if i > columnIndex {
+			skipLength++
+		}
+	}
+	number, err := strconv.Atoi(numberString)
+	if err != nil {
+		panic(err)
+	}
+	return number, skipLength
 }
 
 func round1(file *os.File) int {
@@ -51,38 +116,10 @@ func round1(file *os.File) int {
 }
 
 func adjacentToSymbol(lineNumber, startIndex, numberLength int) bool {
-	var startLine, endLine, startChar, endChar int
+	firstRow, lastRow, firstColumn, lastColumn := getCappedDimensions(lineNumber, startIndex, numberLength)
 
-	// Cap the startline if necessary
-	if lineNumber == 0 {
-		startLine = 0
-	} else {
-		startLine = lineNumber - 1
-	}
-
-	// Cap the endline if necessary
-	if lineNumber == len(engineSchematic)-1 {
-		endLine = len(engineSchematic) - 1
-	} else {
-		endLine = lineNumber + 1
-	}
-
-	// Cap the starting character index if necessary
-	if startIndex == 0 {
-		startChar = 0
-	} else {
-		startChar = startIndex - 1
-	}
-
-	// Cap the ending character index if necessary
-	if startIndex+numberLength == len(engineSchematic[lineNumber]) {
-		endChar = len(engineSchematic[lineNumber]) - 1
-	} else {
-		endChar = startIndex + numberLength
-	}
-
-	for lineIndex := startLine; lineIndex <= endLine; lineIndex++ {
-		for characterIndex := startChar; characterIndex <= endChar; characterIndex++ {
+	for lineIndex := firstRow; lineIndex <= lastRow; lineIndex++ {
+		for characterIndex := firstColumn; characterIndex <= lastColumn; characterIndex++ {
 			char := engineSchematic[lineIndex][characterIndex]
 			if string(char) == "." {
 				continue
@@ -111,4 +148,35 @@ func getNumber(line string, startIndex int) (int, int) {
 		panic(err)
 	}
 	return value, length
+}
+
+func getCappedDimensions(rowIndex, columnIndex, length int) (firstRow, lastRow, firstColumn, lastColumn int) {
+	// Cap the startline if necessary
+	if rowIndex == 0 {
+		firstRow = 0
+	} else {
+		firstRow = rowIndex - 1
+	}
+
+	// Cap the endline if necessary
+	if rowIndex == len(engineSchematic)-1 {
+		lastRow = len(engineSchematic) - 1
+	} else {
+		lastRow = rowIndex + 1
+	}
+
+	// Cap the starting character index if necessary
+	if columnIndex == 0 {
+		firstColumn = 0
+	} else {
+		firstColumn = columnIndex - 1
+	}
+
+	// Cap the ending character index if necessary
+	if columnIndex+length == len(engineSchematic[rowIndex]) {
+		lastColumn = len(engineSchematic[rowIndex]) - 1
+	} else {
+		lastColumn = columnIndex + length
+	}
+	return
 }
