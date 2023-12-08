2b30dcb06f2519f630e1756f73e6197c4cf3ed14
Jan Baer <janthebaer@gmail.com>
2023-12-08 15:10:41 +0100

    day6 round1

---
+++ 06_race/race.go
@@ -0,0 +1,69 @@
+package race
+
+import (
+	"bufio"
+	"strconv"
+	"strings"
+
+	"github.com/JanBdot/advent-of-code-2023/util"
+)
+
+var fileAsSlice []string
+
+func Race() (int, int) {
+	file, err := util.ReadFile("./06_race/input")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fileAsSlice = append(fileAsSlice, scanner.Text())
+	}
+
+	return round1(), round2()
+}
+
+func round2() int {
+	return 0
+}
+
+func round1() int {
+	times := getIntSliceFromString(fileAsSlice[0], ":")
+	distances := getIntSliceFromString(fileAsSlice[1], ":")
+
+	sum := 1
+	for i := 0; i < len(times); i++ {
+		beatingPossibilities := findBeatingPossiblities(times[i], distances[i])
+		sum *= len(beatingPossibilities)
+	}
+
+	return sum
+}
+
+func findBeatingPossiblities(totalTime, minDistance int) []int {
+	var possibilities []int
+	for i := 1; i < totalTime-1; i++ {
+		raceTime := totalTime - i
+		speed := i
+		if speed*raceTime > minDistance {
+			possibilities = append(possibilities, i)
+		}
+	}
+	return possibilities
+}
+
+func getIntSliceFromString(s string, sep string) []int {
+	_, valuesAsString, _ := strings.Cut(s, sep)
+	valuesAsStringSlice := strings.Fields(valuesAsString)
+	values := make([]int, len(valuesAsStringSlice))
+	for i, valueAsString := range valuesAsStringSlice {
+		v, err := strconv.Atoi(valueAsString)
+		if err != nil {
+			panic(err)
+		}
+		values[i] = v
+	}
+	return values
+}
--- main.go
+++ main.go
@@ -3,24 +3,27 @@ package main
 import (
 	"fmt"
 
-	trebuchet "github.com/JanBdot/advent-of-code-2023/01_trebuchet"
-	cubeconundrum "github.com/JanBdot/advent-of-code-2023/02_cube_conundrum"
-	gear_ratios "github.com/JanBdot/advent-of-code-2023/03_gear_ratios"
-	scratchcards "github.com/JanBdot/advent-of-code-2023/04_scratchcards"
-	seed "github.com/JanBdot/advent-of-code-2023/05_seed"
+	// trebuchet "github.com/JanBdot/advent-of-code-2023/01_trebuchet"
+	// cubeconundrum "github.com/JanBdot/advent-of-code-2023/02_cube_conundrum"
+	// gear_ratios "github.com/JanBdot/advent-of-code-2023/03_gear_ratios"
+	// scratchcards "github.com/JanBdot/advent-of-code-2023/04_scratchcards"
+	// seed "github.com/JanBdot/advent-of-code-2023/05_seed"
+	race "github.com/JanBdot/advent-of-code-2023/06_race"
 )
 
 func main() {
 	var round1, round2 int
 	fmt.Println("Advent of Code 2023 by Jan Baer")
-	round1, round2 = trebuchet.Trebuchet()
-	fmt.Printf("Day 01:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
-	round1, round2 = cubeconundrum.CubeConundrum()
-	fmt.Printf("Day 02:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
-	round1, round2 = gear_ratios.GearRatios()
-	fmt.Printf("Day 03:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
-	round1, round2 = scratchcards.Scratchcards()
-	fmt.Printf("Day 04:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
-	round1, round2 = seed.Seed()
-	fmt.Printf("Day 05:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	// round1, round2 = trebuchet.Trebuchet()
+	// fmt.Printf("Day 01:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	// round1, round2 = cubeconundrum.CubeConundrum()
+	// fmt.Printf("Day 02:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	// round1, round2 = gear_ratios.GearRatios()
+	// fmt.Printf("Day 03:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	// round1, round2 = scratchcards.Scratchcards()
+	// fmt.Printf("Day 04:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	// round1, round2 = seed.Seed()
+	// fmt.Printf("Day 05:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
+	round1, round2 = race.Race()
+	fmt.Printf("Day 06:\n\tround 1: %v\n\tround 2: %v\n", round1, round2)
 }
